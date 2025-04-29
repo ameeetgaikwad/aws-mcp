6a86a1d3a72a2a7b8cdf13f9f921a4126403c4da
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2025-04-29 22:38:16 +0530

    route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express from 'express';
-import { errorHandler } from './middlewares/errorHandler';
-
+import express from "express";
+import { errorHandler } from "./middlewares/errorHandler";
+// import winston from 'winston';
+// import expressWinston from 'express-winston';
 const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.all('*', (req, res, next) => {
-    next(new Error(`Can't find ${req.originalUrl} on this server`));
-  });
-  
+// app.all("*", (req, res, next) => {
+//   next(new Error(`Can't find ${req.originalUrl} on this server`));
+// });
+
 // Global error handler (should be after routes)
 app.use(errorHandler);
 
